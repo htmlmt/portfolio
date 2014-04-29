614aaa4a978867e5938073ba9e9d0063aa4568d8
Michael Todd <mikejtodd@gmail.com>
2014-04-28 23:19:26 -0500

    Added draggable needle, fixed coffee

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
+  
+  function getAngle(){
+    var el = document.getElementById("main_target");
+    var st = window.getComputedStyle(el, null);
+    var tr = st.getPropertyValue("-webkit-transform") ||
+             st.getPropertyValue("-moz-transform") ||
+             st.getPropertyValue("-ms-transform") ||
+             st.getPropertyValue("-o-transform") ||
+             st.getPropertyValue("transform") ||
+             "FAIL";
+             
+    if ( tr === "none" ) {
+      css_angle = 0
+      return css_angle
+    } else {
+    var values = tr.split('(')[1].split(')')[0].split(',');
+    var a = values[0];
+    var b = values[1];
+    var c = values[2];
+    var d = values[3];
+
+    var css_angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    
+    return css_angle
+    }
+  };
+  
   var trackOne = $("#track_one_audio")[0];
   var trackTwo = $("#track_two_audio")[0];
   var trackThree = $("#track_three_audio")[0];
@@ -53,19 +80,15 @@ $(document).ready(function(){
   });
   
   $(".coffee").on("click", function(){
-    if ( $(".coffee").hasClass("empty_cup") ) {
-      
-    } else {
-      coffee.play();
-    }
+    coffee.play();
     setTimeout(function(){
       $(".coffee").css("fill", "#CCCCCC")}, 2000);
+    $('.coffee').off();
+    $('.coffee').css("cursor", "default")
   });
   
-  $(".track_one").on("click", function(){
-    if ( $("#record_label").hasClass("record_spin") ) {
-      
-    } else {
+  $('.track_one').click(function(){
+    if ( $("#record_label").hasClass("record_spin") === false ) {
       $("#record_label").addClass("record_spin");
     }
     $(".track").css("opacity", 1.0);
@@ -87,12 +110,13 @@ $(document).ready(function(){
       trackOne.play()}, 500);
     $(".track_one_record").css("stroke", "#666");
     $(".track_one_text").css("fill", "#ad3d32");
+    $("#main_target").css("transition-property", "all");
+    $("#main_target").css("transition-duration", "1s");
+    $('#main_target').css("-webkit-transform", "rotate(27deg)");
   });
   
-  $(".track_two").on("click", function(){
-    if ( $("#record_label").hasClass("record_spin") ) {
-      
-    } else {
+  $('.track_two').click(function(){
+    if ( $("#record_label").hasClass("record_spin") === false ) {
       $("#record_label").addClass("record_spin");
     }
     $(".track").css("opacity", 1.0);
@@ -114,12 +138,13 @@ $(document).ready(function(){
       trackTwo.play()}, 500);
     $(".track_two_record").css("stroke", "#666");
     $(".track_two_text").css("fill", "#ad3d32");
+    $("#main_target").css("transition-property", "all");
+    $("#main_target").css("transition-duration", "1s");
+    $('#main_target').css("-webkit-transform", "rotate(30deg)");
   });
   
-  $(".track_three").on("click", function(){
-    if ( $("#record_label").hasClass("record_spin") ) {
-      
-    } else {
+  $('.track_three').click(function(){
+    if ( $("#record_label").hasClass("record_spin") === false ) {
       $("#record_label").addClass("record_spin");
     }
     $(".track").css("opacity", 1.0);
@@ -141,12 +166,13 @@ $(document).ready(function(){
       trackThree.play()}, 500);
     $(".track_three_record").css("stroke", "#666");
     $(".track_three_text").css("fill", "#ad3d32");
+    $("#main_target").css("transition-property", "all");
+    $("#main_target").css("transition-duration", "1s");
+    $('#main_target').css("-webkit-transform", "rotate(33deg)");
   });
   
-  $(".track_four").on("click", function(){
-    if ( $("#record_label").hasClass("record_spin") ) {
-      
-    } else {
+  $('.track_four').click(function(){
+    if ( $("#record_label").hasClass("record_spin") === false ) {
       $("#record_label").addClass("record_spin");
     }
     $(".track").css("opacity", 1.0);
@@ -168,12 +194,13 @@ $(document).ready(function(){
       trackFour.play()}, 500);
     $(".track_four_record").css("stroke", "#666");
     $(".track_four_text").css("fill", "#ad3d32");
+    $("#main_target").css("transition-property", "all");
+    $("#main_target").css("transition-duration", "1s");
+    $('#main_target').css("-webkit-transform", "rotate(36deg)");
   });
   
-  $(".track_five").on("click", function(){
-    if ( $("#record_label").hasClass("record_spin") ) {
-      
-    } else {
+  $('.track_five').click(function(){
+    if ( $("#record_label").hasClass("record_spin") === false ) {
       $("#record_label").addClass("record_spin");
     }
     $(".track").css("opacity", 1.0);
@@ -195,12 +222,13 @@ $(document).ready(function(){
       trackFive.play()}, 500);
     $(".track_five_record").css("stroke", "#666");
     $(".track_five_text").css("fill", "#ad3d32");
+    $("#main_target").css("transition-property", "all");
+    $("#main_target").css("transition-duration", "1s");
+    $('#main_target').css("-webkit-transform", "rotate(39deg)");
   });
   
-  $(".track_six").on("click", function(){
-    if ( $("#record_label").hasClass("record_spin") ) {
-      
-    } else {
+  $('.track_six').click(function(){
+    if ( $("#record_label").hasClass("record_spin") === false ) {
       $("#record_label").addClass("record_spin");
     }
     $(".track").css("opacity", 1.0);
@@ -222,12 +250,13 @@ $(document).ready(function(){
       trackSix.play()}, 500);
     $(".track_six_record").css("stroke", "#666");
     $(".track_six_text").css("fill", "#ad3d32");
+    $("#main_target").css("transition-property", "all");
+    $("#main_target").css("transition-duration", "1s");
+    $('#main_target').css("-webkit-transform", "rotate(43deg)");
   });
   
-  $(".track_seven").on("click", function(){
-    if ( $("#record_label").hasClass("record_spin") ) {
-      
-    } else {
+  $('.track_seven').click(function(){
+    if ( $("#record_label").hasClass("record_spin") === false ) {
       $("#record_label").addClass("record_spin");
     }
     $(".track").css("opacity", 1.0);
@@ -249,11 +278,15 @@ $(document).ready(function(){
       trackSeven.play()}, 500);
     $(".track_seven_record").css("stroke", "#666");
     $(".track_seven_text").css("fill", "#ad3d32");
+    $("#main_target").css("transition-property", "all");
+    $("#main_target").css("transition-duration", "1s");
+    $('#main_target').css("-webkit-transform", "rotate(46deg)");
   });
   
-  $("#stop").on("click", function(){
-    setTimeout(function(){
-      $("#record_label").removeClass("record_spin")}, 3000);
+  $('#stop').click(function(){
+    $(".record_spin").one('animationiteration webkitAnimationIteration', function() {
+      $("#record_label").removeClass("record_spin");
+    });
     trackOne.pause();
     trackOne.currentTime = 0
     trackTwo.pause();
@@ -270,192 +303,108 @@ $(document).ready(function(){
     trackSeven.currentTime = 0
     $(".track_text").css("fill", "#00B69F");
     $(".track_record").css("stroke", "#3A3A3A");
+    $("#main_target").css("transition-property", "all");
+    $("#main_target").css("transition-duration", "1s");
+    $('#main_target').css("-webkit-transform", "rotate(0deg)");
   });
   
-  $('.track_one').click(function(){
-      var angle = $('#arm').getRotateAngle();
-      if ( angle == 0 ) {
-        turn = 25
-      } else if ( angle == 28 ) {
-        turn = -3
-      } else if ( angle == 31 ) {
-        turn = -6
-      } else if ( angle == 34 ) {
-        turn = -9
-      } else if ( angle == 38 ) {
-        turn = -13
-      } else if ( angle == 41 ) {
-        turn = -16
-      } else if ( angle == 25 ) {
-        turn = 0
-      } else {
-        turn = -19
-      }
-      destination = +angle + +turn;
-      $('#arm').rotate({animateTo:destination});
+  $(document).mousedown(function(){
+    $("#main_target").css("transition-property", "all");
+    $("#main_target").css("transition-duration", "0s");
   });
   
-  $('.track_two').click(function(){
-      var angle = $('#arm').getRotateAngle();
-      if ( angle == 0 ) {
-        turn = 28
-      } else if ( angle == 31 ) {
-        turn = -3
-      } else if ( angle == 34 ) {
-        turn = -6
-      } else if ( angle == 38 ) {
-        turn = -10
-      } else if ( angle == 41 ) {
-        turn = -13
-      } else if ( angle == 44 ) {
-        turn = -16
-      } else if ( angle == 28 ) {
-        turn = 0
-      } else {
-        turn = 3
-      }
-      destination = +angle + +turn;
-      $('#arm').rotate({animateTo:destination});
-  });
-  
-  $('.track_three').click(function(){
-      var angle = $('#arm').getRotateAngle();
-      if ( angle == 0 ) {
-        turn = 31
-      } else if ( angle == 34 ) {
-        turn = -3
-      } else if ( angle == 38 ) {
-        turn = -7
-      } else if ( angle == 41 ) {
-        turn = -10
-      } else if ( angle == 44 ) {
-        turn = -13
-      } else if ( angle == 25 ) {
-        turn = 6
-      } else if ( angle == 31 ) {
-        turn = 0
-      } else {
-        turn = 3
-      }
-      destination = +angle + +turn;
-      $('#arm').rotate({animateTo:destination});
-  });
-  
-  $('.track_four').click(function(){
-      var angle = $('#arm').getRotateAngle();
-      if ( angle == 0 ) {
-        turn = 34
-      } else if ( angle == 38 ) {
-        turn = -4
-      } else if ( angle == 41 ) {
-        turn = -7
-      } else if ( angle == 44 ) {
-        turn = -10
-      } else if ( angle == 25 ) {
-        turn = 9
-      } else if ( angle == 28 ) {
-        turn = 6
-      } else if ( angle == 34 ) {
-        turn = 0
-      } else {
-        turn = 3
-      }
-      destination = +angle + +turn;
-      $('#arm').rotate({animateTo:destination});
-  });
-  
-  $('.track_five').click(function(){
-      var angle = $('#arm').getRotateAngle();
-      if ( angle == 0 ) {
-        turn = 38
-      } else if ( angle == 41 ) {
-        turn = -3
-      } else if ( angle == 44 ) {
-        turn = -6
-      } else if ( angle == 25 ) {
-        turn = 13
-      } else if ( angle == 28 ) {
-        turn = 10
-      } else if ( angle == 31 ) {
-        turn = 7
-      } else if ( angle == 38 ) {
-        turn = 0
-      } else {
-        turn = 4
-      }
-      destination = +angle + +turn;
-      $('#arm').rotate({animateTo:destination});
-  });
-  
-  $('.track_six').click(function(){
-      var angle = $('#arm').getRotateAngle();
-      if ( angle == 0 ) {
-        turn = 41
-      } else if ( angle == 44 ) {
-        turn = -3
-      } else if ( angle == 25 ) {
-        turn = 16
-      } else if ( angle == 28 ) {
-        turn = 13
-      } else if ( angle == 31 ) {
-        turn = 10
-      } else if ( angle == 34 ) {
-        turn = 7
-      } else if ( angle == 41 ) {
-        turn = 0
-      } else {
-        turn = 3
-      }
-      destination = +angle + +turn;
-      $('#arm').rotate({animateTo:destination});
-  });
-  
-  $('.track_seven').click(function(){
-      var angle = $('#arm').getRotateAngle();
-      if ( angle == 0 ) {
-        turn = 44
-      } else if ( angle == 25 ) {
-        turn = 19
-      } else if ( angle == 28 ) {
-        turn = 16
-      } else if ( angle == 31 ) {
-        turn = 13
-      } else if ( angle == 34 ) {
-        turn = 10
-      } else if ( angle == 38 ) {
-        turn = 6
-      } else if ( angle == 44 ) {
-        turn = 0
-      } else {
-        turn = 3
-      }
-      destination = +angle + +turn;
-      $('#arm').rotate({animateTo:destination});
-  });
-  
-  $('#stop').click(function(){
-      var angle = $('#arm').getRotateAngle();
-      if ( angle == 0 ) {
-        turn = 0
-      } else if ( angle == 25 ) {
-        turn = -25
-      } else if ( angle == 28 ) {
-        turn = -28
-      } else if ( angle == 31 ) {
-        turn = -31
-      } else if ( angle == 34 ) {
-        turn = -34
-      } else if ( angle == 38 ) {
-        turn = -38
-      } else if ( angle == 41 ) {
-        turn = -41
-      } else {
-        turn = -44
-      }
-      destination = +angle + +turn;
-      $('#arm').rotate({animateTo:destination});
+  $("#needle").mouseup(function(){
+    var css_angle = getAngle()
+    if ( css_angle > 0 && css_angle < 24 || css_angle > 50 ) {
       $(".record_spin").one('animationiteration webkitAnimationIteration', function() {
         $("#record_label").removeClass("record_spin");
       });
+      trackOne.pause();
+      trackOne.currentTime = 0
+      trackTwo.pause();
+      trackTwo.currentTime = 0
+      trackThree.pause();
+      trackThree.currentTime = 0
+      trackFour.pause();
+      trackFour.currentTime = 0
+      trackFive.pause();
+      trackFive.currentTime = 0
+      trackSix.pause();
+      trackSix.currentTime = 0
+      trackSeven.pause();
+      trackSeven.currentTime = 0
+      $(".track_text").css("fill", "#00B69F");
+      $(".track_record").css("stroke", "#3A3A3A");
+      $("#main_target").css("transition-property", "all");
+      $("#main_target").css("transition-duration", "1s");
+      $('#main_target').css("-webkit-transform", "rotate(0deg)");
+    } else if ( css_angle >= 24 && css_angle < 28 ) {
+      $("#main_target").css("transition-property", "all");
+      $("#main_target").css("transition-duration", "1s");
+      $('#main_target').css("-webkit-transform", "rotate(27deg)");
+      $('.track_one').click();
+    } else if ( css_angle >= 28 && css_angle < 32 ) {
+      $("#main_target").css("transition-property", "all");
+      $("#main_target").css("transition-duration", "1s");
+      $('#main_target').css("-webkit-transform", "rotate(30deg)");
+      $('.track_two').click();
+    } else if ( css_angle >= 32 && css_angle < 35 ) {
+      $("#main_target").css("transition-property", "all");
+      $("#main_target").css("transition-duration", "1s");
+      $('#main_target').css("-webkit-transform", "rotate(33deg)");
+      $('.track_three').click();
+    } else if ( css_angle >= 35 && css_angle < 37 ) {
+      $("#main_target").css("transition-property", "all");
+      $("#main_target").css("transition-duration", "1s");
+      $('#main_target').css("-webkit-transform", "rotate(36deg)");
+      $('.track_four').click();
+    } else if ( css_angle >= 37 && css_angle < 41 ) {
+      $("#main_target").css("transition-property", "all");
+      $("#main_target").css("transition-duration", "1s");
+      $('#main_target').css("-webkit-transform", "rotate(39deg)");
+      $('.track_five').click();
+    } else if ( css_angle >= 41 && css_angle < 44 ) {
+      $("#main_target").css("transition-property", "all");
+      $("#main_target").css("transition-duration", "1s");
+      $('#main_target').css("-webkit-transform", "rotate(43deg)");
+      $('.track_six').click();
+    } else if ( css_angle >= 44 && css_angle < 50 ) {
+      $("#main_target").css("transition-property", "all");
+      $("#main_target").css("transition-duration", "1s");
+      $('#main_target').css("-webkit-transform", "rotate(46deg)");
+      $('.track_seven').click();
+    }
+  });
+  
+  var dragging = false
+
+  $(function() {
+      var target = $('#needle');
+      var mainTarget = $('#main_target');
+  	var offset = mainTarget.offset();
+      target.mousedown(function() {
+          dragging = true
+      })
+      $(document).mouseup(function() {
+          dragging = false
+      })
+      $(document).mousemove(function(e) {
+          if (dragging) {
+
+  			var center_x = (offset.left) + (mainTarget.width()/2);
+  			var center_y = (offset.top) + (mainTarget.height()/2);
+  			var mouse_x = e.pageX; var mouse_y = e.pageY;
+  			var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+  			var degree = (radians * (180 / Math.PI) * -1); 	
+              mainTarget.css('-moz-transform', 'rotate(' + degree + 'deg)');
+              mainTarget.css('-moz-transform-origin', '50% 50%');
+              mainTarget.css('-webkit-transform', 'rotate(' + degree + 'deg)');
+              mainTarget.css('-webkit-transform-origin', '50% 50%');
+              mainTarget.css('-o-transform', 'rotate(' + degree + 'deg)');
+              mainTarget.css('-o-transform-origin', '50% 50%');
+              mainTarget.css('-ms-transform', 'rotate(' + degree + 'deg)');
+              mainTarget.css('-ms-transform-origin', '50% 50%');
+          }
+      })
   });
 });
